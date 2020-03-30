ef4f7ef06a1fd7e0f1cac903289702c1bc665e20
Duponin <pwet@dupon.in>
2020-03-30 04:32:59 +0200

    Formatter did it's job

--- lib/msdb_web/router.ex
+++ lib/msdb_web/router.ex
@@ -2,19 +2,19 @@ defmodule MsdbWeb.Router do
   use MsdbWeb, :router
 
   pipeline :browser do
-    plug :accepts, ["html"]
-    plug :fetch_session
-    plug :fetch_flash
-    plug :protect_from_forgery
-    plug :put_secure_browser_headers
+    plug(:accepts, ["html"])
+    plug(:fetch_session)
+    plug(:fetch_flash)
+    plug(:protect_from_forgery)
+    plug(:put_secure_browser_headers)
   end
 
   pipeline :api do
-    plug :accepts, ["json"]
+    plug(:accepts, ["json"])
   end
 
   scope "/", MsdbWeb do
-    pipe_through :browser
+    pipe_through(:browser)
 
     get("/", PageController, :index)
     get("/zfs", ZfsController, :index)
